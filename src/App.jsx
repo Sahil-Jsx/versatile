507dc3823dcd462e72b7f1b3cc4d2961d6e32c44
abhaym0 <abhaygiri978@gmail.com>
2024-08-09 18:56:46 +0530

    hero chages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./pages/header/header";
+import Hero from "./pages/hero/hero";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Header />
+        <Hero/>
         <Routes>
           <Route path="/" />
         </Routes>
